d7d9563b0731bb7e478180e862f854c6c191b2e1
sachariase99 <sachariase99@hotmail.com>
2024-08-07 23:26:53 +0200

    s

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./components/login";
+import RegisterPage from "./components/register";
 
 function App() {
 
@@ -7,7 +8,9 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path="/" Component={LoginPage}/>
+          <Route path="/login" Component={LoginPage}/>
+          <Route path="/register" Component={RegisterPage}/>
+
         </Routes>
       </Router>
     </>
